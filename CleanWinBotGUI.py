c6e833996c48b871685d7e9232ce0bd2d4f48c92
Kaili <kailisun25@gmail.com>
2022-03-27 23:22:52 +0800

    create an exe file

--- CleanWinBotGUI.py
+++ CleanWinBotGUI.py
@@ -1,29 +1,25 @@
-from cgitb import text
 from cleanWinBot import RemoveFileFrom,getPathFromJson
 from tkinter import *
 from pathlib import Path
 
 pathDict = getPathFromJson('setting.json')
 
-
-
-
 def clicked():
     for index,jsnPath in enumerate(pathDict):
         removepath = Path(jsnPath['pathloc'])
-        print(removepath)
+        #print(removepath)
         list.insert(0,removepath)
         for res in enumerate(RemoveFileFrom(removepath)):
-            print(res)
-        list.insert(0,res)
+            #print(res)
+            list.insert(0,res)
 
 
 window = Tk()
 window.title("Welcome Windows Cleaner")
-window.geometry('350x200')
+window.geometry('500x400')
 btn = Button(window, text="Clean Your PC",command=clicked)
 btn.grid(column=2, row=0)
 btn.pack()
-list = Listbox(window,bg = 'white',width = 50)
+list = Listbox(window,bg = 'white',width = 90,height=90)
 list.pack()
 window.mainloop()
